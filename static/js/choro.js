45837e074cd535a298b6db4da6b5b4be37c5cb5a
Michelle Salter <mlw1116d1@gmail.com>
2020-03-11 20:44:10 -0400

    heroku

--- static/js/choro.js
+++ static/js/choro.js
@@ -1,4 +1,4 @@
-api_key = 'sk.eyJ1IjoibWxzYWx0ZXIiLCJhIjoiY2s2N2N6YTh5MHFiazNzbnM0YzR3bnBueSJ9.HBHK3aCTTq-Kca86GwdWqw'
+from config import api_key
 
 // Creating map object
 var myMap = L.map("choromap", {
